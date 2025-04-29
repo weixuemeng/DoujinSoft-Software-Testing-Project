bfb7ffd8a9f68b5f0c2bd08d079a5e072975df32
Yihang Wang <31209506+wyh2001@users.noreply.github.com>
2025-04-28 21:33:37 -0400

    Add more GUI tests

--- test/com/difegue/doujinsoft/TestGUI.java
+++ test/com/difegue/doujinsoft/TestGUI.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,6 +18,7 @@ import java.io.File;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class TestGUI {
 
@@ -53,13 +55,194 @@ public class TestGUI {
         Assertions.assertTrue(files != null && files.length > 0);
     }
 
+    // This is a faq page
     @Test
     public void testAboutPage() {
         driver.get("https://diy.tvc-16.science/about");
-//            String ADMIN_USERNAME = "admin";
-//    String ADMIN_PASSWORD = "VO£w790LL3[D(ijxM|GN";
-//        String BASE_URL   = "https://"+ ADMIN_USERNAME +":"+ ADMIN_PASSWORD +"@doujinsoft.yihang.one/manage";
-//        driver.get(BASE_URL);
+        Assertions.assertTrue(Objects.requireNonNull(driver.getPageSource()).contains("Questions"));
     }
 
+    @Test
+    public void testIndexPage() {
+        driver.get("https://diy.tvc-16.science/");
+        Assertions.assertTrue(Objects.requireNonNull(driver.getPageSource()).contains("DoujinSoft Store"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"games", "records", "comics"})
+    public void testCart(String type) {
+        driver.get("https://diy.tvc-16.science/" + type);
+        WebElement cartNumElement = driver.findElement(By.xpath("/html/body/div[1]/nav/div/ul[1]/li[4]/a/span/b"));
+        String cartNum1 = cartNumElement.getText();
+        Assertions.assertEquals("0", cartNum1);
+        List<WebElement> addToCartButton = driver.findElements(By.xpath("//a[@data-tooltip='Add to cart']"));
+        Assertions.assertFalse(addToCartButton.isEmpty());
+        addToCartButton.get(0).click();
+        WebElement cartNumElement2 = driver.findElement(By.xpath("/html/body/div[1]/nav/div/ul[1]/li[4]/a/span/b"));
+        String cartNum2 = cartNumElement2.getText();
+        Assertions.assertEquals("1", cartNum2);
+    }
+
+    //Get sharing link
+    @ParameterizedTest
+    @ValueSource(strings = {"games", "records", "comics"})
+    public void testSharing(String type) throws InterruptedException {
+        driver.get("https://diy.tvc-16.science/" + type);
+        List<WebElement> sharingButtons = driver.findElements(By.xpath("//a[@data-tooltip='Get sharing link']"));
+        Assertions.assertFalse(sharingButtons.isEmpty());
+        sharingButtons.get(0).click();
+        Thread.sleep(1000); // for demo, I could use js to get clipboard content but that needs manual permission confirmation
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "games, JON+",
+            "records, Ben",
+            "comics, Spencer",
+    })
+    public void testSearchingCreatorName(String type, String creatorName) throws InterruptedException {
+        driver.get("https://diy.tvc-16.science/" + type);
+        WebElement creatorInput = driver.findElement(By.xpath("//*[@id=\"maker_name\"]"));
+        creatorInput.sendKeys(creatorName);
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"game-search\"]/a[1]"));
+        searchButton.click();
+        WebElement contentList = driver.findElement(By.xpath("//*[@id=\"content\"]"));
+        Thread.sleep(1000); // wait for the page to load
+        String notFoundText = "No more "+ type +".\n" + "Sad.";
+        Assertions.assertFalse(contentList.getText().contains(notFoundText));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "games, Tennis Pro",
+            "records, 0apple!",
+            "comics, 2 Cats",
+    })
+    public void testSearchingWorkName(String type, String creatorName) throws InterruptedException {
+        driver.get("https://diy.tvc-16.science/" + type);
+        WebElement workInput = driver.findElement(By.xpath("//*[@id=\"item_name\"]"));
+        workInput.sendKeys(creatorName);
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"game-search\"]/a[1]"));
+        searchButton.click();
+        WebElement contentList = driver.findElement(By.xpath("//*[@id=\"content\"]"));
+        Thread.sleep(1000); // wait for the page to load
+        String notFoundText = "No more "+ type +".\n" + "Sad.";
+        Assertions.assertFalse(contentList.getText().contains(notFoundText));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "games, JON+xxxxxxxxxxx",
+            "records, Benxxxxxxxxxxxxx",
+            "comics, Spencexxxxxxxxxxxxxxxxxxr",
+    })
+    public void testSearchingCreatorNameWithNotValidName(String type, String creatorName) throws InterruptedException {
+        driver.get("https://diy.tvc-16.science/" + type);
+        WebElement creatorInput = driver.findElement(By.xpath("//*[@id=\"maker_name\"]"));
+        creatorInput.sendKeys(creatorName);
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"game-search\"]/a[1]"));
+        searchButton.click();
+        WebElement contentList = driver.findElement(By.xpath("//*[@id=\"content\"]"));
+        Thread.sleep(1000); // wait for the page to load
+        String notFoundText = "No more "+ type +".\n" + "Sad.";
+        Assertions.assertTrue(contentList.getText().contains(notFoundText));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "games, Tennis Proaaaaaaaaaaaaaaaa",
+            "records, 0apple!aaaaaaaaaaaaaaaaaaa",
+            "comics, 2 Catsaaaaaaaaaaaaaaaaaa",
+    })
+    public void testSearchingWorkNameWithNotValidName(String type, String creatorName) throws InterruptedException {
+        driver.get("https://diy.tvc-16.science/" + type);
+        WebElement workInput = driver.findElement(By.xpath("//*[@id=\"item_name\"]"));
+        workInput.sendKeys(creatorName);
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"game-search\"]/a[1]"));
+        searchButton.click();
+        WebElement contentList = driver.findElement(By.xpath("//*[@id=\"content\"]"));
+        Thread.sleep(1000); // wait for the page to load
+        String notFoundText = "No more "+ type +".\n" + "Sad.";
+        Assertions.assertTrue(contentList.getText().contains(notFoundText));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "games, JON+, Tennis Pro",
+            "records, Ben, 8Bit Melody",
+            "comics, Spencer, 2 Cats",
+    })
+    public void testSearchingCreatorNameAndWorkName(String type, String creatorName, String workName) throws InterruptedException {
+        driver.get("https://diy.tvc-16.science/" + type);
+        WebElement creatorInput = driver.findElement(By.xpath("//*[@id=\"maker_name\"]"));
+        creatorInput.sendKeys(creatorName);
+        WebElement workInput = driver.findElement(By.xpath("//*[@id=\"item_name\"]"));
+        workInput.sendKeys(workName);
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"game-search\"]/a[1]"));
+        searchButton.click();
+        WebElement contentList = driver.findElement(By.xpath("//*[@id=\"content\"]"));
+        Thread.sleep(1000); // wait for the page to load
+        String notFoundText = "No more "+ type +".\n" + "Sad.";
+        Assertions.assertFalse(contentList.getText().contains(notFoundText));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "games, JON+aaa, Tennis Pro",
+            "records, Benaaa, 8Bit Melody",
+            "comics, Spenceraaa, 2 Cats",
+    })
+    public void testSearchingCreatorNameAndWorkNameWithNotValidName(String type, String creatorName, String workName) throws InterruptedException {
+        driver.get("https://diy.tvc-16.science/" + type);
+        WebElement creatorInput = driver.findElement(By.xpath("//*[@id=\"maker_name\"]"));
+        creatorInput.sendKeys(creatorName);
+        WebElement workInput = driver.findElement(By.xpath("//*[@id=\"item_name\"]"));
+        workInput.sendKeys(workName);
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"game-search\"]/a[1]"));
+        searchButton.click();
+        WebElement contentList = driver.findElement(By.xpath("//*[@id=\"content\"]"));
+        Thread.sleep(1000); // wait for the page to load
+        String notFoundText = "No more "+ type +".\n" + "Sad.";
+        Assertions.assertTrue(contentList.getText().contains(notFoundText));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "games, JON+, Tennis Proaaa",
+            "records, Ben, 8Bit Melodaaa",
+            "comics, Spencer, 2 Catsaaa",
+    })
+    public void testSearchingCreatorNameAndWorkNameWithNotValidName2(String type, String creatorName, String workName) throws InterruptedException {
+        driver.get("https://diy.tvc-16.science/" + type);
+        WebElement creatorInput = driver.findElement(By.xpath("//*[@id=\"maker_name\"]"));
+        creatorInput.sendKeys(creatorName);
+        WebElement workInput = driver.findElement(By.xpath("//*[@id=\"item_name\"]"));
+        workInput.sendKeys(workName);
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"game-search\"]/a[1]"));
+        searchButton.click();
+        WebElement contentList = driver.findElement(By.xpath("//*[@id=\"content\"]"));
+        Thread.sleep(1000); // wait for the page to load
+        String notFoundText = "No more "+ type +".\n" + "Sad.";
+        Assertions.assertTrue(contentList.getText().contains(notFoundText));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "games, JON+aaa, Tennis Proaaa",
+            "records, Benaaa, 8Bit Melodaaa",
+            "comics, Spenceraaa, 2 Catsaaa",
+    })
+    public void testSearchingCreatorNameAndWorkNameWithNotValidName3(String type, String creatorName, String workName) throws InterruptedException {
+        driver.get("https://diy.tvc-16.science/" + type);
+        WebElement creatorInput = driver.findElement(By.xpath("//*[@id=\"maker_name\"]"));
+        creatorInput.sendKeys(creatorName);
+        WebElement workInput = driver.findElement(By.xpath("//*[@id=\"item_name\"]"));
+        workInput.sendKeys(workName);
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"game-search\"]/a[1]"));
+        searchButton.click();
+        WebElement contentList = driver.findElement(By.xpath("//*[@id=\"content\"]"));
+        Thread.sleep(1000); // wait for the page to load
+        String notFoundText = "No more "+ type +".\n" + "Sad.";
+        Assertions.assertTrue(contentList.getText().contains(notFoundText));
+    }
 }
